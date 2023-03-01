9b2452a044979a79ecf8f9ec361bd14de6622fc6
shovonmustafa <121207652+shovonmustafa@users.noreply.github.com>
2023-03-01 16:34:24 -0500

    event listener

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
-console.log("Your index.js file is loaded correctly!");
+// Sample code from homework
+/* console.log("Your index.js file is loaded correctly!"); */
+
+// Sample code from homework
+/* $("#myID").on("click", function(){
+    console.log("You clicked on #myID");
+}); */
+
+const button = document.querySelector("button[type='button']");
+button.addEventListener("click", function() {
+    window.alert("Coming soon!");
+});
